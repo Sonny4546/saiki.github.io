8c7dcf68029a43ec74a08f89bf06cd0b658f5e05
Saiki <76763287+Sonny4546@users.noreply.github.com>
2023-10-29 18:58:00 +0800

    glow fix and spotify

--- brn.js
+++ brn.js
@@ -3,3 +3,12 @@ let vid = document.getElementById("bgvideo");
 function AudioToggle() { 
     vid.muted = !vid.muted;
 } 
+
+let glow = document.getElementById("tlGlow");
+
+function GlowToggle() {
+    glow.style.animation = "none";
+    glow.style.animation = "titleGlow 0.75s";
+    window.location.href = '#section2';
+    setTimeout(() => {  glow.style.animation = "none"; }, 1000);
+}
