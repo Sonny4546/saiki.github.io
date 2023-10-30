fe025400f6e8b0dc2583c9f0fbe330ac57d38027
Saiki <76763287+Sonny4546@users.noreply.github.com>
2023-10-30 15:28:51 +0800

    title upload

--- brn.js
+++ brn.js
@@ -4,11 +4,29 @@ function AudioToggle() {
     vid.muted = !vid.muted;
 } 
 
-let glow = document.getElementById("tlGlow");
+let btn = document.getElementById("btn");
 
-function GlowToggle() {
+(function () {
+    btn.style.display = "none";
+    setTimeout(() => {btn.style.display = "";}, 1750);
+}) ();
+
+let glow = document.getElementById("tlGlow");
+let animt = document.getElementById("title");
+let fade = document.getElementById("tlfade");
+let drop = document.getElementById("dropsh");
+
+function TitleClick() {
     glow.style.animation = "none";
-    glow.style.animation = "titleGlow 0.75s";
-    window.location.href = '#section2';
-    setTimeout(() => {  glow.style.animation = "none"; }, 1000);
+    glow.style.animation = "titleGlow 1s";
+    animt.style.transform = "scale(1.2)";
+    drop.style.transform = "scale(1.2)";
+    fade.style.opacity = "25%";
+    fade.style.transform = "translateY(-230px) skewx(-10deg)";
+    setTimeout(() => {  window.location.href = '#section2'; }, 1000);
+    setTimeout(() => {  animt.style.transform = "scale(1)"; }, 1000);
+    setTimeout(() => {  drop.style.transform = "scale(1)"; }, 1000);
+    setTimeout(() => {  fade.style.opacity = ""; }, 750);
+    setTimeout(() => {  fade.style.transform = "translateY(-230px)"; }, 1750);
+    setTimeout(() => {  glow.style.animation = "none"; }, 1750);
 }
